670fc30a825d8ad960ba13114c8b9a2d4fa9beca
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-04-27 20:38:09 +0530

    removed logs

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const verify = function () {
   const name = document.querySelector('#entered-name').value.trim();
   if (!name) return;
-  console.log(name);
   document.querySelector('#main').className = 'hide';
   if (name.toLowerCase() !== 'raja') {
     return document.querySelector('#wrong').classList.remove('hide');
